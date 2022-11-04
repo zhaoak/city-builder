516191d25398ace8529dea412f1f93a96b872c9e
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-03 17:43:54 -0700

    added dom elements to js

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 
 /* Get DOM Elements */
 const welcomeBanner = document.getElementById('welcome-banner');
+const geographyImg = document.getElementById('geography-img');
+const architectureImg = document.getElementById('architecture-img');
+const weatherImg = document.getElementById('weather-img');
 const sloganLocation = document.getElementById('slogan-location');
+const geographySelect = document.getElementById('geography-select');
+const architectureSelect = document.getElementById('architecture-select');
+const weatherSelect = document.getElementById('weather-select');
+const cityTextbox = document.getElementById('city-textbox');
+const cityButton = document.getElementById('city-button');
+const sloganTextbox = document.getElementById('slogan-textbox');
+const sloganButton = document.getElementById('slogan-button');
 
 /* State */
 
